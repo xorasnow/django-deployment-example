ba8c409302dd6c144140914552668ec78f4123b9
xora snow <xs@yacht365.net>
2017-04-23 19:12:30 -0700

    first

---
+++ learn_user/user_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from user_app import views
+
+# TEMPLATE URLS!
+
+
+app_name = 'user_app'
+
+urlpatterns = [
+    url(r'^register/$',views.register,name='register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+
+]
